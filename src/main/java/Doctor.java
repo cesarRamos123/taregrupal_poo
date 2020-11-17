033bb31a063af41d8803bf1653e39f562e11b84c
cesar <javieramos895@gmail.com>
2020-11-17 16:57:03 -0600

    coomit repositorio

---
+++ src/main/java/Doctor.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Javier Ramos
+ */
+public class Doctor  extends Persona {
+   
+public Doctor (){
+    setNombre("jose");
+    setApellido("pineda");
+    setIdentidad("888888");
+    setTelefono("7848484");
+    setCorreo("hdhj@gmail.com");
+}
+}
